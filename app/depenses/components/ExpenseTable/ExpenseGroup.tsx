5794b7ae78364a0b7bea9e650ce2d128f026e74f
hussein <husseinsedoum@gmail.com>
2025-02-04 22:34:40 +0000

    update  12 (1)

--- app/depenses/components/ExpenseTable/ExpenseGroup.tsx
+++ app/depenses/components/ExpenseTable/ExpenseGroup.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { TableRow, TableCell } from "@/components/ui/table";
 import { ExpenseRow } from "./ExpenseRow";
 import { Expense } from "@/lib/types";
@@ -7,6 +6,7 @@ import { Expense } from "@/lib/types";
 interface ExpenseGroupProps {
     monthYear: string;
     expenses: Expense[];
+    total: number; // Total for the month
     editingId: string | null;
     formData: Expense;
     onEdit: (expense: Expense) => void;
@@ -20,6 +20,7 @@ interface ExpenseGroupProps {
 export const ExpenseGroup = ({
     monthYear,
     expenses,
+    total,
     editingId,
     formData,
     onEdit,
@@ -31,11 +32,14 @@ export const ExpenseGroup = ({
 }: ExpenseGroupProps) => {
     return (
         <>
+            {/* Month Header */}
             <TableRow className="bg-gray-100">
-                <TableCell colSpan={6} className="font-bold text-primary capitalize">
+                <TableCell colSpan={6} className="font-bold text-primary capitalize text-lg">
                     {monthYear}
                 </TableCell>
             </TableRow>
+
+            {/* Expense Rows */}
             {expenses.map((expense) => (
                 <ExpenseRow
                     key={String(expense._id)}
@@ -50,6 +54,17 @@ export const ExpenseGroup = ({
                     deletingId={deletingId}
                 />
             ))}
+
+            <TableRow>
+                <TableCell colSpan={6}>
+                    <div className="p-3 bg-gray-50  rounded-md shadow-sm">
+                        <div className="flex justify-between">
+                            <span className="font-bold text-primary text-lg">{`Total pour ${monthYear}:`}</span>
+                            <span className="text-gray-600 font-medium text-lg">{total.toLocaleString()} MRU</span>
+                        </div>
+                    </div>
+                </TableCell>
+            </TableRow>
         </>
     );
 };
